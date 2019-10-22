5a3d08cde5a5e95262a91860bcc79309f1858d90
Vitor Carrilho <vitor.carrilho@hotmail.com>
2019-10-22 20:44:41 -0300

    fixs

--- InserirContato.php
+++ InserirContato.php
@@ -1,25 +1,38 @@
 <?php
 require_once('navbar.php');
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+</head>
+
+<body>
+
 <div class='container' style="margin-top: 5%">
 
-    <form action="../Src/Controllers/PessoaController.php" method="GET" >
+    <form action="../Src/Controllers/PessoaController.php" method="POST" >
     <div class="form-row">
         <div class="form-group col-md-6">
         <label for="nome">Nome</label>
-        <input type="text" class="form-control" id="nome" placeholder="Nome">
+        <input type="text" class="form-control" name="nome" placeholder="Nome">
         </div>
         <div class="form-group col-md-6">
         <label for="endereco">Endereço</label>
-        <input type="text" class="form-control" id="endereco" placeholder="Endereço...">
+        <input type="text" class="form-control" name="endereco" placeholder="Endereço...">
         </div>
     </div>
     <div class="form-group">
         <label for="telefone">Telefone</label>
-        <input type="text" class="form-control" id="telefone" placeholder="(99) 9 99999-9999">
+        <input type="text" class="form-control" name="telefone" placeholder="(99) 9 99999-9999">
     </div>
     
     <button type="submit" class="btn btn-primary">Cadastrar</button>
     </form>
 
 </div>
+
+</body>
+
+</html>
--- src/Controllers/PessoaController.php
+++ src/Controllers/PessoaController.php
@@ -2,4 +2,14 @@
 
 Class PessoaController {
     
+    private $nome, $endereco, $telefone;
+
+    public function __construnct(){
+        $this->nome = $_POST['nome'];
+        teste();
+    }
+
+    public function teste(){
+        echo 'hello';
+    }
 }
